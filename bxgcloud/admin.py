a91314332ffcfe287506a4861d0e74c06e7b1591
Shaun-Chen <shaun.chen94@gmail.com>
2016-09-29 00:59:39 +0800

    add the DL for member

--- bxgcloud/admin.py
+++ bxgcloud/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import *
 # Register your models here.
+
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('mid', 'name', 'sex', 'birth', 'phone_number', 'email', 'wxunionid', 'bonus_point', 'living_city', 'profession', 'summary', 'web', 'picture', 'register_time')
+
+admin.site.register(Member,MemberAdmin)
--- bxgcloud/models.py
+++ bxgcloud/models.py
@@ -3,3 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    SEX_TYPE = (
+        (1,'boy'),
+        (2,'girl'),
+    )
+    mid = models.SmallIntegerField(primary_key=True)
+    name = models.CharField(max_length=128)
+    sex = models.SmallIntegerField(default=1,choices=SEX_TYPE)
+    birth = models.DateField()
+    phone_number = models.CharField(max_length=128, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    wxunionid = models.CharField(max_length=128, blank=True, null=True)
+    bonus_point = models.SmallIntegerField(blank=True, null=True)
+    living_city = models.CharField(max_length=1024, blank=True, null=True)
+    profession = models.CharField(max_length=256, blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    web = models.URLField(blank=True, null=True)
+    picture = models.ImageField(blank=True, null=True)
+    register_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = True
+        db_table = "Member"
+        ordering = ['name', 'birth', 'bonus_point', 'register_time']
+        unique_together = ('name', 'email', 'phone_number', 'wxunionid')
+
+    def __unicode__(self):
+        return self.mid + '.' +self.name
