78f995a519ac41e9d17b4e7584a04a09a7c0f433
Guo Chen <chenguo@honovation.com>
2016-10-11 22:22:45 +0800

    feat: homepage html

--- bxgcloud/views.py
+++ bxgcloud/views.py
@@ -12,44 +12,49 @@ from rest_framework import status
 from .models import *
 from .serializers import *
 
+
+def homepage(request):
+    return render(request, 'index.html')
+
+
 # Create your views here.
 class MemberList(APIView):
-	'''
-	the Member List about all member infomations
-	'''
-	def get(self, request):
-		'''
-		display all member informations
-		:param request: none
-		:return: serializer.data
-		'''
-		members = Member.objects.all()
-		serializer = MemberSerializer(members, many=True)
-		return Response({'result':'ok', 'data':serializer.data})
-	
-	def post(self, request):
-		'''
-		add the member for Member table
-		:param request: request.data
-		:return: result
-		'''
-		serializers = MemberSerializer(data = request.data)
-		if serializers.is_valid():
-			serializers.save()
-		else:
-			result = {'result': 'the author post failed',
-					  'reason': 'the message is not valid'}
-			return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-		
-	def delete(self,requset):
-		'''
-		delete the member from Member table
-		:param requset:
-		:return:
-		'''
-		try:
-			Member.objects.all().delete()
-		except:
-			traceback.print_exc()
-			return Response(status.HTTP_403_FORBIDDEN)
-		
+    '''
+    the Member List about all member infomations
+    '''
+
+    def get(self, request):
+        '''
+        display all member informations
+        :param request: none
+        :return: serializer.data
+        '''
+        members = Member.objects.all()
+        serializer = MemberSerializer(members, many=True)
+        return Response({'result': 'ok', 'data': serializer.data})
+
+    def post(self, request):
+        '''
+        add the member for Member table
+        :param request: request.data
+        :return: result
+        '''
+        serializers = MemberSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+        else:
+            result = {'result': 'the author post failed',
+                      'reason': 'the message is not valid'}
+            return Response(result, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, requset):
+        '''
+        delete the member from Member table
+        :param requset:
+        :return:
+        '''
+        try:
+            Member.objects.all().delete()
+        except:
+            traceback.print_exc()
+            return Response(status.HTTP_403_FORBIDDEN)
