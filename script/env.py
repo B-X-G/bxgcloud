48b22d7c4ac046fb73deb18aa08019a65882024f
Guo Chen <chenguo@honovation.com>
2016-10-12 23:54:00 +0800

    refactor: review install package

--- script/env.py
+++ script/env.py
@@ -4,20 +4,35 @@ from __future__ import unicode_literals, print_function, division
 import subprocess
 import os
 
+requirements_path = '{}/requirements.txt'.format(os.getcwd())
 
-def install_package(name):
-    os.system('sudo pip install {}'.format(name))
-    result = subprocess.Popen('sudo pip freeze', stdout=subprocess.PIPE, shell=True)
-    output = result.communicate()[0]
-    package_names = output.split('\n')
-    for package_name in package_names:
-        if package_name.lower().startswith(name.lower()):
-            with open('{}/requirements.txt'.format(os.getcwd()), 'a+') as r:
-                all_packages = set(readline.split('\n')[0] for readline in r.readlines())
-                if package_name in all_packages:
-                    print ('the package is exist')
-                else:
-                    r.write(package_name)
-                break
+
+def install_package(package_name):
+    """
+    安装python包规则：
+        1. 检查系统是否已安装该包
+        2. 如果系统已安装该包，则检查已装的版本号与 “requirements.txt” 中对应的版本号
+        3. 如果系统已安装该包并 “requirements.txt” 已记录该包名称及版本号，但已装的版本号与 “requirements.txt” 中对应的版本号不一致，则按照 “requirements.txt” 中重新装包
+        4. 如果系统已安装该包并 “requirements.txt” 未记录该包名称及版本号，则直接将包的名称及版本号记录到 “requirements.txt” 中
+        5. 如果系统未安装该包并 “requirements.txt” 已记录该包名称及版本号，则根据 “requirements.txt” 的记录安装包
+        6. 如果系统未安装该包并 “requirements.txt” 未记录该包名称及版本号，则安装指定版本包（未指定版本则默认为最新包）并将包的名称和版本号记录到 “requirements.txt” 中
+    """
+    package_name_term = package_name if '==' in package_name else '{}=='.format(package_name)
+    exist_package_name_and_version_in_env = subprocess.Popen('sudo pip freeze |grep -i {}'.format(package_name_term), stdout=subprocess.PIPE, shell=True).communicate()[0]
+    with open(requirements_path, 'a+') as r:
+        exist_package_name_and_versions = set(line.split('\n')[0] for line in r.readlines())
+        exist_package_name_and_version_list = [name_and_version for name_and_version in exist_package_name_and_versions if name_and_version.lower().startswith(package_name_term.lower())]
+        exist_package_name_and_version = exist_package_name_and_version_list[0].strip() if exist_package_name_and_version_list else None
+        if exist_package_name_and_version:
+            if exist_package_name_and_version.strip() == exist_package_name_and_version_in_env.strip():
+                print('you already install this package')
+            else:
+                subprocess.Popen('sudo pip install {}'.format(exist_package_name_and_version), stdout=subprocess.PIPE, shell=True)
+                print('successfully change the package version by requirements.txt')
         else:
-            continue
+            if exist_package_name_and_version_in_env:
+                r.write(exist_package_name_and_version_in_env)
+                print('successfully record package name and version in requirements.txt')
+            new_package_name_version = subprocess.Popen('sudo pip install {}; sudo pip freeze |grep {}'.format(package_name, package_name_term), stdout=subprocess.PIPE, shell=True).communicate()[0]
+            r.write(new_package_name_version)
+            print('successfully install {}'.format(new_package_name_version))
